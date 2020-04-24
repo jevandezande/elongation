Fixes units, now all in SI.

elongation/elongation.py
@@ -15,15 +15,18 @@ class Elongation:
 
         :param xs: elongation (in units of strain)
         :param ys: force (in Newtons)
+        :param gauge_length: length of sample (in meters)
+        :param sample width: width of sample (in meters)
+        :param sample_thickness: thickness of sample (in meters)
         :param name: optional name for the Elongation
         """
         assert len(xs) == len(ys)
 
         self.xs = np.array(xs)
         self.ys = np.array(ys)
-        self.gauge_length = gauge_length
-        self.sample_width = sample_width  # mm
-        self.sample_thickness = sample_thickness  # mm
+        self.gauge_length = gauge_length  # m
+        self.sample_width = sample_width  # m
+        self.sample_thickness = sample_thickness  # m
         self.name = name
 
     def __eq__(self, other):
@@ -76,9 +79,9 @@ class Elongation:
         """
         Cross sectional area of the material.
 
-        :return: cross_section in mm^2
+        :return: cross_section in m²
         """
-        return self.sample_thickness*self.sample_width  # mm^2
+        return self.sample_thickness*self.sample_width # m x m = m²
 
     def smoothed(self, box_pts=True):
         """
@@ -179,13 +182,13 @@ class Elongation:
 
         :return: Young's modulus array (units of Pa)
         """
-        return self.derivative()/self.cross_section  # N/L·ΔL * A
+        return self.derivative()/self.cross_section  # N/ΔL · L₀/A
 
     def derivative(self):
         """
         :return: derivative
         """
-        return np.diff(self.ys)/np.diff(self.xs)  # N/L·ΔL
+        return np.diff(self.ys)/np.diff(self.xs)  # ΔN/ΔL · L₀
 
     def peaks(self, **kwargs):
         """
@@ -295,7 +298,7 @@ Sample Width, {e.sample_width}
 Sample Thickness, {e.sample_thickness}
 
 Points
-   mm,       N""")
+   %,       N""")
         for x, y in zip(e.xs, e.ys):
             f.write(f'\n{x:>8.4f}, {y:>8.4f}')
 
@@ -533,9 +536,9 @@ Doc={MT2500:14|
         elongations.append(
             Elongation(
                 xs, data['ys'],
-                float(data['gauge_length']),
-                float(data['sample_width']),
-                float(data['sample_thickness']),
+                float(data['gauge_length']) / 1e3,  # mm → m
+                float(data['sample_width']) / 1e3,  # mm → m
+                float(data['sample_thickness']) / 1e3,  # mm → m
                 None
             )
         )

tests/test_elongation.py
@@ -18,21 +18,21 @@ def test_read():
 def test_read_prn():
     e0, e1, e2 = read_prn('tests/test_files/test1.prn')
 
-    assert e0.sample_thickness == 1.000
-    assert e0.sample_width == 1.000
-    assert e0.gauge_length == 6.000
+    assert e0.sample_thickness == 0.01
+    assert e0.sample_width == 0.01
+    assert e0.gauge_length == 0.06
     assert len(e0.xs) == 2344
     assert len(e0.xs) == len(e0.ys)
 
-    assert e1.sample_thickness == 1.000
-    assert e1.sample_width == 1.000
-    assert e1.gauge_length == 60.000
+    assert e1.sample_thickness == 0.01
+    assert e1.sample_width == 0.01
+    assert e1.gauge_length == 0.06
     assert len(e1.xs) == 73
     assert len(e1.xs) == len(e1.ys)
 
-    assert e2.sample_thickness == 1.000
-    assert e2.sample_width == 1.000
-    assert e2.gauge_length == 60.000
+    assert e2.sample_thickness == 0.01
+    assert e2.sample_width == 0.01
+    assert e2.gauge_length == 0.06
     assert len(e2.xs) == 65
     assert len(e2.xs) == len(e2.ys)
 
@@ -61,7 +61,7 @@ def test_copy():
 
 def test_cross_section():
     elong = read_prn('tests/test_files/test1.prn')[0]
-    assert elong.cross_section == 1
+    assert elong.cross_section == 0.0001
 
 
 def test_smoothed():
@@ -107,13 +107,13 @@ def test_youngs_modulus_array():
     elong = read_prn('tests/test_files/test1.prn')[0]
     for val in elong.youngs_modulus_array:
         if val is np.nan:
-            print(9)
-    aae(elong.youngs_modulus_array[:7], [0, 0.4067902, 0, 0, 0, 0.5543513, 0])
+            pass
+    aae(elong.youngs_modulus_array[:7], [0, 4067.9019681, 0, 0, 0, 5543.5134664, 0])
 
 
 def test_youngs_modulus():
     elong = read_prn('tests/test_files/test1.prn')[0]
-    aae(elong.youngs_modulus, 1.4498419835141907)
+    aae(elong.youngs_modulus, 14498.419835141907)
 
 
 def test_break():